b1bc724994f00c09864c29efb2f32fb3a3a43418
JuanDa-code <juandax123456@gmail.com>
2023-09-05 12:40:57 -0500

    Reasignacion y redeclaracion

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; // Undefined
+firstName = 'Juan';
+console.log(firstName);
+
+var lastName = 'David'; // Declarar / Asignar
+lastName = 'Ana'; // Reasignar
+console.log(lastName);
+
+var secondName = 'David'; // Declarando / Asignando
+var secondName = 'Ana'; // Reasignado
+console.log(secondName);
+
+// Let
+let fruit = 'Apple'; // Declarar y asignar
+fruit = 'Kiwi'; // Reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+// Const
+const animal = 'Dog'; // Declara y asigna
+// animal = 'cat'; // Reasignando
+const animal = 'Snake';
+console.log(animal);
+
+
+const vehicles = [];
+vehicles.push('🚗');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
